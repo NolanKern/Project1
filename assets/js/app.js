067eea120eebf9b1fee75856b2852ff71461b6d1
Roland Willis <rolandwillisiv@gmail.com>
2018-04-05 13:23:11 -0700

    added year validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,3 +85,28 @@ $(document).on("click", "#run-search", function() {
     
 
 });
+
+function yearValidation(year,ev) {
+
+    var text = /^[0-9]+$/;
+    if(ev.type=="blur" || year.length==4 && ev.keyCode!=8 && ev.keyCode!=46) {
+      if (year != 0) {
+          if ((year != "") && (!text.test(year))) {
+  
+              //alert("Please Enter Numeric Values Only");
+              return false;
+          }
+  
+          if (year.length != 4) {
+              //alert("Year is not proper. Please check");
+              return false;
+          }
+          var current_year=new Date().getFullYear();
+          if((year < 1920) || (year > current_year))
+              {
+              //alert("Year should be in range 1920 to current year");
+              return false;
+              }
+          return true;
+      } }
+  }
